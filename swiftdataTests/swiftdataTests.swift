95721b88e4781517ed02c04163ceda6b83331870
Lazar Otasevic <>
2023-11-17 23:02:43 +0100

    added tests for combine

--- swiftdataTests/swiftdataTests.swift
+++ swiftdataTests/swiftdataTests.swift
@@ -17,8 +17,24 @@ extension Inspection: InspectionEmissary { }
 
 @MainActor final class swiftdataTests: XCTestCase {
     let modelContainer: ModelContainer = .makeModel()
+    
+    func testSwiftDataWithCombine() throws {
+        var model = SwiftDataModel()
+        let exp = model.inspection.inspect { view in
+            let model = try view.actualView()
+            XCTAssertEqual(model.items.count, 0)
+            model.addItem()
+            XCTAssertEqual(model.items.count, 1)
+            model.deleteItems(offsets: .init(integer: 0))
+            XCTAssertEqual(model.items.count, 0)
+        }
+        ViewHosting.host(
+            view: model.environment(\.modelContext, modelContainer.mainContext)
+        )
+        wait(for: [exp], timeout: 1)
+    }
 
-    func testSwiftData() throws {
+    func testSwiftDataWithAppear() throws {
         var model = SwiftDataModel()
         let exp = model.on(\.didAppear) { view in
             let model = try view.actualView()
